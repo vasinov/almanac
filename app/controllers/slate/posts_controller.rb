e5c283a95ae8bf036f1f8b746be009c27cf83c05
Vasily Vasinov <des.elyon@gmail.com>
2012-10-10 20:50:21 -0600

    Adding current_user handling in the posts controller.

--- app/controllers/slate/posts_controller.rb
+++ app/controllers/slate/posts_controller.rb
@@ -1,7 +1,18 @@
 module Slate
   class PostsController < ApplicationController
-    def index
+    respond_to :html
 
+    def index
+      @posts = (defined? current_user) ? "posts" : nil
+
+      respond_with(@posts) do |format|
+        if @posts.nil?
+          flash.now[:alert] = 'current_user is not found. Check your Devise setup.'
+          format.html
+        else
+          format.html
+        end
+      end
     end
   end
 end
