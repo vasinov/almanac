9ae0ac634d2636587029d7d9b156bd9b14b8a56d
Vasily Vasinov <des.elyon@gmail.com>
2012-11-16 17:35:52 -0700

    Fixed routes issues.

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,23 @@ Almanac::Engine.routes.draw do
   root :to => "posts#index"
 
   match "new_blog" => "blogs#new", :as => "new_blog"
+  match "edit_blog/:id" => "blogs#edit", :as => "edit_blog"
   resources :blogs do
     member do
       get "spam" => "blogs#spam"
     end
   end
 
-  #match "feed" => "posts#index", :format => "rss"
-  #match ":id/edit" => "posts#edit", :as => "edit_post"
-  #match "new" => "posts#new", :as => "new_post"
-  #match "drafts/:id" => "posts#draft", :as => "draft", :via => :get
-  #match ":slug" => "posts#show", :as => "post", :via => :get
-  #match "posts" => "posts#create", :as => "posts", :via => :post
-  #match ":id/edit" => "posts#update", :as => "posts", :via => :put
-  #match ":id" => "posts#destroy", :as => "post", :via => :delete
-  resources :posts, :path => '' do
-    member do
-      get "drafts/:id" => "posts#draft", :as => "draft", :via => :get
-    end
+  match "feed" => "posts#index", :format => "rss"
+  match ":id/edit" => "posts#edit", :as => "edit_post"
+  match "new" => "posts#new", :as => "new_post"
+  match "drafts/:id" => "posts#draft", :as => "draft", :via => :get
+  match ":slug" => "posts#show", :as => "post", :via => :get
+  match "posts" => "posts#create", :as => "posts", :via => :post
+  match ":id" => "posts#update", :as => "posts", :via => :put
+  match "posts" => "posts#update", :as => "posts", :via => :put
+  match ":id" => "posts#destroy", :as => "post", :via => :delete
+  resources :posts do
     resources :images
     resources :comments
   end
